393dec592b9691e8ef43f0dac938d03145860c98
ArthurLopes191 <arthurlopesreal@gmail.com>
2026-01-29 18:31:29 -0300

    estudando testes unitários

---
+++ Curso-TS/my-app-ts/src/services/soma.test.tsx
@@ -0,0 +1,18 @@
+import {soma, multiplica} from './soma'
+
+describe('soma', () => {
+    it('deve somar 1 ao número informado', () => {
+        const value = soma(1)
+        expect(value).toBe(2)
+    })
+
+    it('deve multiplicar o número por 2', () => {
+        const value = multiplica(2, 2)
+        expect(value).toBe(4)
+    })
+
+    it('deve multiplicar o numero por 3', () => {
+        const value = multiplica(2, 3)
+        expect(value).toBe(6)
+    })
+})
